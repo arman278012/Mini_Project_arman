bd673630a3d454dd44ea724950d2449447f8f1f9
m-triple-m <triplem656@gmail.com>
2022-04-09 12:23:14 +0530

    ..

--- Front-End(S-1)/secondapp/src/App.js
+++ Front-End(S-1)/secondapp/src/App.js
@@ -1,38 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './Components/header';
-import {BrowserRouter,Route,Routes} from 'react-router-dom';
-import Gallery from './Components/gallery';
-import Login from './Components/studentslogin';
-import Photo from './Components/photo';
-import TeacherLogin from './Components/teacherslogin';
-import TeacherSignup from './Components/techerSignup';
-import BatchDashboard from './Components/batchDashboard';
-import AddNew from './Components/AddNewAssignment';
-import AboutUs from './Components/aboutus';
-import ListBatches from './Components/listBatches';
-import Accord from './Components/accordion';
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./Components/header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Gallery from "./Components/gallery";
+import Login from "./Components/studentslogin";
+import Photo from "./Components/photo";
+import TeacherLogin from "./Components/teacherslogin";
+import TeacherSignup from "./Components/techerSignup";
+import BatchDashboard from "./Components/batchDashboard";
+import AddNew from "./Components/AddNewAssignment";
+import AboutUs from "./Components/aboutus";
+import ListBatches from "./Components/listBatches";
+import Accord from "./Components/accordion";
 function App() {
   return (
     <div>
-      
       <BrowserRouter>
-      <Home></Home>
-      <Routes>
-        {/* <Route element={} path="/header"></Route> */}
-        <Route element={<Gallery></Gallery>} path="/gallery"></Route>
-        {/* <Route element={<Login></Login>} path="/login"></Route> */}
-        <Route element={<Photo></Photo>} path="/home"></Route>
-        <Route element={<TeacherLogin></TeacherLogin>} path="/teacherlogin"></Route>
-        <Route element={<TeacherSignup></TeacherSignup>} path="/teachersignup"></Route>
-        <Route element={<BatchDashboard></BatchDashboard>} path="/batchdashboard"></Route>
-        <Route element={<AddNew></AddNew>} path="/addnew"></Route>
-        <Route element={<AboutUs></AboutUs>} path="/about"></Route>
-        <Route element={<ListBatches></ListBatches>} path="/listbatch"></Route>
-        <Route element={<Accord></Accord>} path="/accord"></Route>
-      </Routes> 
-        </BrowserRouter>
-      
+        <Home></Home>
+        <Routes>
+          {/* <Route element={} path="/header"></Route> */}
+          <Route element={<Gallery></Gallery>} path="/gallery"></Route>
+          {/* <Route element={<Login></Login>} path="/login"></Route> */}
+          <Route element={<Photo></Photo>} path="/home"></Route>
+          <Route
+            element={<TeacherLogin></TeacherLogin>}
+            path="/teacherlogin"
+          ></Route>
+          <Route
+            element={<TeacherSignup></TeacherSignup>}
+            path="/teachersignup"
+          ></Route>
+          <Route
+            element={<BatchDashboard></BatchDashboard>}
+            path="/batchdashboard"
+          ></Route>
+          <Route element={<AddNew></AddNew>} path="/addnew"></Route>
+          <Route element={<AboutUs></AboutUs>} path="/about"></Route>
+          <Route
+            element={<ListBatches></ListBatches>}
+            path="/listAssignment"
+          ></Route>
+          <Route element={<Accord></Accord>} path="/accord"></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- Front-End(S-1)/secondapp/src/Components/teacherslogin.js
+++ Front-End(S-1)/secondapp/src/Components/teacherslogin.js
@@ -1,148 +1,150 @@
-import { Button, Card, CardContent, Checkbox, Grid, Paper, TextField } from "@mui/material"
+import {
+  Button,
+  Card,
+  CardContent,
+  Checkbox,
+  Grid,
+  Paper,
+  TextField,
+} from "@mui/material";
 import { type } from "@testing-library/user-event/dist/type";
 import { Formik } from "formik";
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import app_config from "./config";
 import { userContext } from "./userContext";
 
 const TeacherLogin = () => {
+  const url = app_config.api_url;
 
-    const url = app_config.api_url;
+  const navigate = useNavigate();
 
-    // const { setCurrentUser, setloggedin } = useContext(userContext)
+  // const { setCurrentUser, setloggedin } = useContext(userContext)
 
-    const loginForm = {
-        username: "",
-        password: "",
-    };
-    const loginSubmit = (values) => {
-        console.log(values);
+  const loginForm = {
+    username: "",
+    password: "",
+  };
+  const loginSubmit = (values) => {
+    console.log(values);
 
+    fetch(url + "/teacher/checklogin", {
+      method: "POST",
+      body: JSON.stringify(values),
+      headers: { "Content-Type": "application/json" },
+    }).then((res) => {
+      console.log(res.status);
+      if (res.status === 200) {
+        // setLoggedin(true)
+        Swal.fire({
+          icon: "success",
+          title: "Success",
+          text: "loggedIn successfully",
+        }).then(() => {
+          res.json().then((data) => {
+            sessionStorage.setItem("user", JSON.stringify(data));
+            navigate("/listAssignment");
+          });
+        });
+      } else if (res.status === 300) {
+        Swal.fire({
+          icon: "error",
+          title: "Failed",
+          text: "Login Failed",
+        });
+      }
+    });
 
-            fetch(url + '/teacher/checklogin', {
-            method: "POST",
-            body: JSON.stringify(values),
-            headers: {
-                "content-type": "applicaton/json"
-            }
-        })
+    // if (res.status === 200) {
+    //     setloggedin(true)
+    //     Swal.fire({
+    //       icon: "success",
+    //       title: "Success",
+    //       text: "Loggedin Successfully",
+    //     });
 
+    //   } else if (res.status === 300) {
+    //     Swal.fire({
+    //       icon: "error",
+    //       title: "Failed",
+    //       text: "Loggedin Failed",
+    //     });
+    //   }
 
-        .then(res =>{
-            console.log(res.status)
-            if(res.status===200){
-                setLoggedin(true)
-                Swal.fire({
-                    icon:"success",
-                    title:"Success",
-                    text:"loggedIn successfully"
-                })
-            }else if(res.status === 300){
-                Swal.fire({
-                    icon:'error',
-                    title:'Failed',
-                    text:'Login Failed'
-                })
-            }
-            
-        })
+    // .then ( (res) => {
+    //     console.log(res.status)
+    //     if (res.status === 200) {
+    //         Swal.fire({
+    //             icon: 'success',
+    //             title: 'Success',
+    //             text: 'LoggedIn successfully'
+    //         });
+    //     }
+    //     else if (res.status === 300) {
+    //         Swal.fire({
+    //             icon: 'error',
+    //             title: 'Failed',
+    //             text: 'LoggedIn failed'
+    //         });
+    //     }
+    // }
+    // )
+  };
 
-
-        // if (res.status === 200) {
-        //     setloggedin(true)
-        //     Swal.fire({
-        //       icon: "success",
-        //       title: "Success",
-        //       text: "Loggedin Successfully",
-        //     });
-    
-    
-        //   } else if (res.status === 300) {
-        //     Swal.fire({
-        //       icon: "error",
-        //       title: "Failed",
-        //       text: "Loggedin Failed",
-        //     });
-        //   }
-
-
-
-
-        // .then ( (res) => {
-        //     console.log(res.status)
-        //     if (res.status === 200) {
-        //         Swal.fire({
-        //             icon: 'success',
-        //             title: 'Success',
-        //             text: 'LoggedIn successfully'
-        //         });
-        //     }
-        //     else if (res.status === 300) {
-        //         Swal.fire({
-        //             icon: 'error',
-        //             title: 'Failed',
-        //             text: 'LoggedIn failed'
-        //         });
-        //     }
-        // }
-        // )
-    };
-
-    return (
-        <div className="mt-3 student_login">
-            <Paper className="background">
-                <Grid container justifyContent="center">
-                    <Grid item md={3}>
-                        <Card>
-                            <CardContent className="stu">
-
-                                <Formik initialValues={loginForm} onSubmit={loginSubmit}>
-                                    {({ values, handleChange, handleSubmit }) =>
-                                        <form onSubmit={handleSubmit}>
-                                            <p className="h3 text-center mb-5 mt-3">Teacher Login-Here</p>
-                                            <TextField
-                                                className="teacher_username w-100 mt-3"
-                                                label="teacher's username"
-                                                variant="outlined"
-                                                id="username"
-                                                onChange={handleChange}
-                                                value={values.username}
-                                            />
-                                            <TextField
-                                                className="mt-3 w-100 teacher_pass"
-                                                type="password"
-                                                label="teacher's password"                                      
-                                                variant="outlined"
-                                                id="password"
-                                                onChange={handleChange}
-                                                value={values.password}
-                                            /> <br />
-                                            <span><Checkbox defaultChecked size="small" />Remember me</span>
-                                            <a className="forgot_passowrd">forgot password?</a>
-
-                                            <Button
-                                                type="submit"
-                                                color="error"
-                                                variant="contained"
-                                                className="mt-3 login_btn">
-                                                Login to Continue
-                                            </Button>
-                                        </form>
-                                    }
-
-                                </Formik>
-
-
-
-                            </CardContent>
-                        </Card>
-                    </Grid>
-
-                </Grid>
-            </Paper>
-        </div>
-
-    )
-}
-export default TeacherLogin
+  return (
+    <div className="mt-3 student_login">
+      <Paper className="background">
+        <Grid container justifyContent="center">
+          <Grid item md={3}>
+            <Card>
+              <CardContent className="stu">
+                <Formik initialValues={loginForm} onSubmit={loginSubmit}>
+                  {({ values, handleChange, handleSubmit }) => (
+                    <form onSubmit={handleSubmit}>
+                      <p className="h3 text-center mb-5 mt-3">
+                        Teacher Login-Here
+                      </p>
+                      <TextField
+                        className="teacher_username w-100 mt-3"
+                        label="teacher's username"
+                        variant="outlined"
+                        id="username"
+                        onChange={handleChange}
+                        value={values.username}
+                      />
+                      <TextField
+                        className="mt-3 w-100 teacher_pass"
+                        type="password"
+                        label="teacher's password"
+                        variant="outlined"
+                        id="password"
+                        onChange={handleChange}
+                        value={values.password}
+                      />{" "}
+                      <br />
+                      <span>
+                        <Checkbox defaultChecked size="small" />
+                        Remember me
+                      </span>
+                      <a className="forgot_passowrd">forgot password?</a>
+                      <Button
+                        type="submit"
+                        color="error"
+                        variant="contained"
+                        className="mt-3 login_btn"
+                      >
+                        Login to Continue
+                      </Button>
+                    </form>
+                  )}
+                </Formik>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+  );
+};
+export default TeacherLogin;
