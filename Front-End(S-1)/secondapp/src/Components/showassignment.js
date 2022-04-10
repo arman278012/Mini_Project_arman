78e57b7f8f76cfcf7321804dff6b46a4a5b5e997
arman278012 <aa278012@gmail.com>
2022-04-10 12:08:23 +0530

    ...

--- Front-End(S-1)/secondapp/src/Components/showassignment.js
+++ Front-End(S-1)/secondapp/src/Components/showassignment.js
@@ -1,30 +1,80 @@
 import { Accordion, AccordionDetails, AccordionSummary, Button, Card, CardContent, Grid, Paper, TextField } from "@mui/material"
-import { Form } from "formik"
+import { Form, Formik } from "formik"
+import app_config from "./config";
 
 const ShowAssignment = () => {
+    const url = app_config.api_url;
+
+    const SubjectForm = {
+        batch: "",
+        subject: "",
+      };
+
+
+    //   const showAssign = (values) => {
+    //     console.log(values);
+        
+    //   fetch(url + "/showassignment/checklogin", {
+    //     method: "POST",
+    //     body: JSON.stringify(values),
+    //     headers: { "Content-Type": "application/json" },
+    //   }).then((res) => {
+    //     console.log(res.status);
+    //     if (res.status === 200) {
+          
+    //       Swal.fire({
+    //         icon: "success",
+    //         title: "Success",
+    //         text: "Thanku!!",
+    //       })
+    //     } else if (res.status === 300) {
+    //       Swal.fire({
+    //         icon: "error",
+    //         title: "Failed",
+    //         text: "Login Failed",
+    //       });
+    //     }
+    //   });
+    // }
+
+
+
+
     return (
+
+        
         <div>
-            <form>
+            <Formik initialValues={SubjectForm} onSubmit={showAssign}>
+            {({values,handleChange,handleSubmit})=>(
+                <form onSubmit={handleSubmit}>
                 <Grid container spacing={10}>
 
                     <Grid item md={6}>
                         <TextField
                             className="w-100 mt-5"
                             label="Enter your batch here"
+                            onChange={handleChange}
+                            value={values.batch}
                         />
                     </Grid>
 
                     <Grid item md={6}>
                         <TextField
                             className="w-100 mt-5"
-                            label="Enter your batch here"
+                            label="Enter your subject here"
+                            onChange={handleChange}
+                            value={values.subject}
                         />
                     </Grid>
                 </Grid>
                 
                <Button className="mt-5 show_button w-30" variant="contained">Show Assignments</Button>
+            </form>
+            )}
 
-                <Accordion className="mt-5">
+            </Formik>
+
+            <Accordion className="mt-5">
                     <AccordionSummary>
                         Assignments
                     </AccordionSummary>
@@ -35,7 +85,6 @@ const ShowAssignment = () => {
                      <h1>arman</h1>
                     </AccordionDetails>
                 </Accordion>
-            </form>
         </div>
     )
 }
